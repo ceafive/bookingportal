7690b155495c892740dc2b1b00eb8b891eb2c02d
Castro Agbo <castro.agbo@paykode.com>
2021-07-22 10:57:21 +0000

    init

--- api/app.js
+++ api/app.js
@@ -35,4 +35,12 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../build")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../build", "index.html"));
+  });
+}
+
 module.exports = app;
