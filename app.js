03ab5fd9aaae1d1b2ba3282b88477ad64338f2bb
ceafive <ceafive@gmail.com>
2022-02-03 16:51:07 +0000

    fixes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
@@ -40,7 +39,6 @@ app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use("/api", indexRouter);
 
